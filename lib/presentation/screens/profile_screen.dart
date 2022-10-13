ca0a1dd3f0c7a16212400d5023cca0cb5b774288
vgne <vusal.h.mammadov@gmail.com>
2022-10-13 07:44:11 +0300

    Avatar

--- lib/presentation/screens/profile_screen.dart
+++ lib/presentation/screens/profile_screen.dart
@@ -7,7 +7,26 @@ class ProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile'),
+        centerTitle: false,
+        elevation: 0,
+        title: const Text(
+          'Account',
+          style: TextStyle(fontWeight: FontWeight.w700, fontSize: 24.0),
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 18.0),
+          child: Image.asset('assets/images/profile_icon.png'),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18.0),
+            child: Row(
+              children: [],
+            ),
+          ),
+        ),
       ),
     );
   }
